0acc78acc165ff09ae3001650eecd0da734cbb8b
skyhills13 <skyhills13@gmail.com>
2017-05-16 16:39:16 +0900

    테스트 환경 셋팅

--- src/test/java/com/skyhills/dummy/repository/PetRepositoryCustomImplTest.java
+++ src/test/java/com/skyhills/dummy/repository/PetRepositoryCustomImplTest.java
@@ -1,11 +1,13 @@
 package com.skyhills.dummy.repository;
 
+import com.skyhills.dummy.DummyApplication;
 import com.skyhills.dummy.domain.dto.PetResponse;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
@@ -14,6 +16,7 @@ import java.util.List;
  * Created by soeunpark on 2017. 5. 16..
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DummyApplication.class)
 public class PetRepositoryCustomImplTest {
     Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -23,7 +26,7 @@ public class PetRepositoryCustomImplTest {
     @Test
     public void findByPetName() throws Exception {
         List<PetResponse> petList  = petRepository.findByPetName("aaa");
-        logger.debug("petList : {}", petList);
+        logger.debug("########petList######### : {}", petList);
     }
 
 }
